385b53c22415e7713e5ee3477dd262bcfa758867
D0p3-CoRe <sylwester.kupczynski@gmail.com>
2024-09-06 13:22:27 +0200

    Initial Commit

---
+++ DailyAssistant/DailyAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyAssistantApp.swift
+//  DailyAssistant
+//
+//  Created by Sylwester Kupczyński on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct DailyAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
